c8ad772310be25dd2fb88dc03c141faf77102d6b
hwasyui <angelicasutiwhiharto@gmail.com>
2025-08-04 13:31:31 +0700

    Adding the log out button in both dashboard

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 
 function AdminDashboard() {
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        navigate('/login');
+    };
   return (
-    <div>AdminDashboard</div>
+    <div>AdminDashboard
+        <button onClick={handleLogout} className="block w-full text-left px-4 py-2 text-red-500 hover:bg-green-100 dark:hover:bg-green-500">Log Out</button>
+    </div>
   )
 }
 
--- frontend/src/pages/DeveloperDashboard.jsx
+++ frontend/src/pages/DeveloperDashboard.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 
 function DeveloperDashboard() {
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        navigate('/login');
+    };
   return (
-    <div>DeveloperDashboard</div>
+    <div>
+        Developer Dashboard
+        <button onClick={handleLogout} className="block w-full text-left px-4 py-2 text-red-500 hover:bg-green-100 dark:hover:bg-green-500">Log Out</button>
+    </div>
+    
   )
 }
 
